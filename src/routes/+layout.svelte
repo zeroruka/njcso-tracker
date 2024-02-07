299396e6dd21317319c95f101cd3f89be3fe0979
HaoChen <github@haochen.dev>
2024-02-07 20:28:20 +0800

    remove vite-pwa

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,23 +1,8 @@
 <script lang="ts">
 	import Splashscreens from '$lib/components/splashscreens.svelte';
 	import { Toaster } from '$lib/components/ui/sonner';
-	import { onMount } from 'svelte';
-	import { pwaInfo } from 'virtual:pwa-info';
 	import '../app.pcss';
-
-	onMount(async () => {
-		if (pwaInfo) {
-			const { registerSW } = await import('virtual:pwa-register');
-			registerSW({ immediate: true });
-		}
-	});
-
-	$: webManifest = pwaInfo ? pwaInfo.webManifest.linkTag : '';
 </script>
-
-<svelte:head>
-	{@html webManifest}
-</svelte:head>
 
 <Splashscreens />
 <Toaster />
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 import { defineConfig } from 'vite';
 import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
-	plugins: [
-		mkcert(),
-		sveltekit(),
-		SvelteKitPWA({
-			registerType: 'autoUpdate',
-			manifestFilename: 'site.webmanifest'
-		})
-	]
+	plugins: [mkcert(), sveltekit()]
 });
