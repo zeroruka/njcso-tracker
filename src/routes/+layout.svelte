0d60e42c684c8e0665e6471b5553eb53bd528ab0
HaoChen <github@haochen.dev>
2024-02-08 11:57:07 +0800

    speed insights and analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
+	import { dev } from '$app/environment';
 	import Splashscreens from '$lib/components/splashscreens.svelte';
 	import { Toaster } from '$lib/components/ui/sonner';
+	import { inject } from '@vercel/analytics';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 	import '../app.pcss';
+
+	inject({ mode: dev ? 'development' : 'production' });
+	injectSpeedInsights();
 </script>
 
 <Splashscreens />
